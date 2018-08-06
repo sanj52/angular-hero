b973ed9743b0bb6bb47e3a5e6b0866844dbb5690
sanj52 <shtsanjay52@gmail.com>
2018-08-06 16:08:47 +0545

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,13 @@ import { HeroComponent } from './modules/hero/pages/hero/hero.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent
+    HeroComponent  //added 
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent],
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ] // added for custom Seclector <app-hero>
 })
 export class AppModule { }
